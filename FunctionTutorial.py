e360e3a53b26d3a3978f655cbac2540ecee92850
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2020-09-01 11:48:22 -0700

    Initial commit

---
+++ FunctionTutorial.py
@@ -0,0 +1,25 @@
+#Q1
+def area(width, height):
+    result = width * height
+    return result
+#Q2
+result = area(5, 6)
+print(result)
+#Q3
+print(area(6,9))
+print(area(1,1))
+#Q4
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+#Q5
+print(subtract(1,2))
+print(subtract(2,1))
+#Q6
+def divide(num1,num2):
+    return (num1/num2)
+#Q7
+def add(num1,num2):
+    return(num1+num2)
+print(add(1,1))
+print(add(1,2))
